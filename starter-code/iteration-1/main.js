e73110b8aeb8fed27f7579d1ef9ffcd2c698eebc
Tommy DiLillo <tommydilillo@gmail.com>
2018-08-02 22:07:23 -0400

    iteration 1 complete

---
+++ starter-code/iteration-1/main.js
@@ -0,0 +1,29 @@
+let tasks = [];
+function addTask(task) {
+    tasks.push(task);
+    console.log(`Task "` + task + `" inserted in the list`);
+    return tasks.length;
+}
+console.log(`Number of items: ` + addTask);
+function listAllTasks() {
+    for (let i of tasks) {
+        console.log(`List all tasks: ${i}`);
+    }
+}
+function deleteTask(task) {
+    let index = tasks.indexOf(task);
+    console.log(index);
+    tasks.splice(index, 1);
+    console.log(`Task "` + task + `" deleted from the list`);
+    console.log(tasks);
+    return tasks.length;
+}
+console.log(`Number of items: ` + deleteTask);
+console.log("Number of items:", addTask("This is our first task"));
+console.log("Number of items:", addTask("Eat pizza 🍕 yummy!!!"));
+console.log("Number of items:", addTask("Finish this iteration 1!! 🤓"));
+console.log("Number of items:", addTask("Finish this iteration 2!! 🤓"));
+console.log("Number of items:", addTask("Finish this iteration 3!! 🤓"));
+listAllTasks();
+console.log("Number of items:", deleteTask("Finish this iteration 1!! 🤓"));
+listAllTasks();
